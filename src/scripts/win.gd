76afa6f853ffe9011c7bc3f96f8f147c8c5cb950
Alstocras <alstoperiment@gmail.com>
2026-04-12 13:01:42 +0000

    Refactored code to make it work in the new filestructure

--- src/scripts/win.gd
+++ src/scripts/win.gd
@@ -2,4 +2,4 @@ extends StaticBody2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://end.tscn");
+		get_tree().change_scene_to_file("res://src/scenes/end.tscn");
