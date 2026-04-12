3f4fb533b48ff2fc31f555eef9c7e37bd5cb9204
Alstocras <alstoperiment@gmail.com>
2026-04-12 06:23:14 +0000

    added a little helper text

--- win.gd
+++ win.gd
@@ -1,8 +1,5 @@
 extends StaticBody2D
 
-@export var levelNum: int;
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		var filePath = "res://level" + str(levelNum + 1) + ".tscn"
-		get_tree().change_scene_to_file(filePath);
+		get_tree().change_scene_to_file("res://end.tscn");
