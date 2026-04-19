51acc1dd6cc178389eceb3eb380f20e92f365d42
Alstocras <alstoperiment@gmail.com>
2026-04-19 13:41:20 +0000

    yay slider

--- src/scripts/level-canvas-layer.gd
+++ src/scripts/level-canvas-layer.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
-@onready var forceIndicator: RichTextLabel = $ForceIndicatorBase
+@onready var forceX: RichTextLabel = $ForceX
+@onready var forceY: RichTextLabel = $ForceY
 
 func _process(delta: float) -> void:
-	forceIndicator.text = "F = " + str(round(Global.playerForce.y)) + " kg⋅px⋅s⁻²";
+	forceX.text = "Fx = " + str(round(Global.playerForce.x)) + " kg⋅px⋅s⁻²";
+	forceY.text = "Fy = " + str(round(Global.playerForce.y)) + " kg⋅px⋅s⁻²";
